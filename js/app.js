a5a4118e217b3328abdf34afb7027ea9471c4223
Eduardo ALves Ferreira <edualvesf13@gmail.com>
2020-11-25 20:34:47 -0300

    Add function startGame and direction of the snake.

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ let canvas = document.getElementById('snake')
 let context = canvas.getContext('2d') // contexto de renderização em 2D para a superfície de desenho do elemento 
 let box = 32
 let snake = []
+let direction = 'right'
+let game = setInterval(startGame, 100)
 
 snake[0] = {
     x: 8 * box,
@@ -20,5 +22,24 @@ function criarSnake() {
     }
 }
 
-criarBG()
-criarSnake()
+function startGame() {
+    criarBG()
+    criarSnake()
+
+    let snakeX = snake[0].x
+    let snakeY = snake[0].y
+
+    if (direction == 'right') snakeX += box
+    if (direction == 'left') snakeX -= box
+    if (direction == 'up') snakeY -= box
+    if (direction == 'down') snakeY += box
+
+    snake.pop()
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead)
+}
